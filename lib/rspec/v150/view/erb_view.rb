cdcabdcaa3d60cb786f11f3e86dcb50f37aca98f
Ægir Örn Símonarson <agirorn@gmail.com>
2012-11-04 22:11:44 +0000

    Fixing: exeption shen specs run from rspec command as a suite

--- lib/rspec/v150/view/erb_view.rb
+++ lib/rspec/v150/view/erb_view.rb
@@ -1,32 +1,34 @@
 require 'action_view'
+require 'rspec/v150/view'
+module RSpec::V150
+  module View
+    class ErbView
+      Erubis = ActionView::Template::Handlers::Erubis
 
-module RSpec::V150::View
-  class ErbView
-    Erubis = ActionView::Template::Handlers::Erubis
+      def render(path)
+        self.template = path
+        view.result(binding())
+      end
 
-    def render(path)
-      self.template = path
-      view.result(binding())
-    end
+      def view
+        Erubis.new(template)
+      end
 
-    def view
-      Erubis.new(template)
-    end
+      def template
+        File.readlines(@template).join()
+      end
 
-    def template
-      File.readlines(@template).join()
-    end
+      def template=(value)
+        @template = value
+      end
 
-    def template=(value)
-      @template = value
-    end
+      def output_buffer
+        @output_buffer ||= ActionView::OutputBuffer.new
+      end
 
-    def output_buffer
-      @output_buffer ||= ActionView::OutputBuffer.new
-    end
-
-    def assign(name, variable)
-      instance_variable_set("@#{name}".to_sym, variable)
+      def assign(name, variable)
+        instance_variable_set("@#{name}".to_sym, variable)
+      end
     end
   end
 end
