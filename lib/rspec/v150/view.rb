3ba53ddbcf3ec220214657c9ac6015e34c5fe182
Ægir Örn Símonarson <agirorn@gmail.com>
2012-11-04 14:26:30 +0000

    Adding basic rendering support.

--- lib/rspec/v150/view.rb
+++ lib/rspec/v150/view.rb
@@ -1,6 +1,53 @@
+require 'action_view'
+
 module RSpec
   module V150
     module View
+      def view
+        @_view ||= ErbView.new
+      end
+
+      def render
+        @rendered = view.render
+      end
+
+      def rendered
+        @rendered
+      end
+
+      def template
+        example.example_group.top_level_description
+      end
+
+      def assign(name, variable)
+        view.assign(name, variable)
+      end
+
+      private
+
+      class ErbView
+        Erubis = ActionView::Template::Handlers::Erubis
+
+        def render
+          view.result(binding())
+        end
+
+        def view
+          Erubis.new(template)
+        end
+
+        # def template
+
+        # end
+
+        def output_buffer
+          @output_buffer ||= ActionView::OutputBuffer.new
+        end
+
+        def assign(name, variable)
+          instance_variable_set("@#{name}".to_sym, variable)
+        end
+      end
     end
   end
 end
--- spec/rspec/v150/view_spec.rb
+++ spec/rspec/v150/view_spec.rb
@@ -1,4 +1,42 @@
 require 'rspec/v150/view'
 
 describe RSpec::V150::View do
+  let(:simple_template) { 'Hello <%= "World" %>' }
+  let(:example_group) { double(:top_level_description => 'post/index') }
+  let(:example) { double(:example_group => example_group) }
+
+  let(:view_spec) do
+    Class.new do
+      include RSpec::V150::View
+    end.new
+  end
+
+  it 'responds to view' do
+    view_spec.should respond_to(:view)
+  end
+
+  it 'responds to render' do
+    view_spec.should respond_to(:render)
+  end
+
+  it 'responds to rendered' do
+  end
+
+  it 'renders a simple view' do
+    view_spec.view.stub(:template) { simple_template }
+    view_spec.render
+    view_spec.rendered.should == 'Hello World'
+  end
+
+  it 'asks rspec for the template name' do
+    view_spec.stub(:example => example)
+    view_spec.template.should == 'post/index'
+  end
+
+  it "set's instance varables for the view" do
+    post = double
+    view_spec.assign(:post, post)
+    view_spec.view.instance_variable_get(:@post).should eq(post)
+  end
+
 end
