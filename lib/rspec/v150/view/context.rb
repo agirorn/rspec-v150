7e9d7070e9d587640723beba876548edc5628d23
Ægir Örn Símonarson <agirorn@gmail.com>
2012-11-05 15:41:35 +0000

    Fix indentation

--- lib/rspec/v150/view/context.rb
+++ lib/rspec/v150/view/context.rb
@@ -1,18 +1,18 @@
 module RSpec::V150::View
-    class Context
-      include ActionView::Helpers
-      include ActionView::Helpers::CaptureHelper
+  class Context
+    include ActionView::Helpers
+    include ActionView::Helpers::CaptureHelper
 
-      def _render_view(view)
-        view.result(binding())
-      end
-
-      def assign(name, variable)
-        instance_variable_set("@#{name}".to_sym, variable)
-      end
-
-      def output_buffer
-        @output_buffer ||= ActionView::OutputBuffer.new
-      end
+    def _render_view(view)
+      view.result(binding())
     end
+
+    def assign(name, variable)
+      instance_variable_set("@#{name}".to_sym, variable)
+    end
+
+    def output_buffer
+      @output_buffer ||= ActionView::OutputBuffer.new
+    end
+  end
 end
